4dde8698207a7408f19f0dee352e958a485e9caa
dmalt <dm.altukhov@ya.ru>
2024-12-05 21:20:51 +0100

    add day3 input & day4 solution

--- day4/solution.py
+++ day4/solution.py
@@ -22,7 +22,25 @@ def count_xmas(text, i, j):
     res = sum("".join(c) == "XMAS" for c in candidates)
     return res
 
+
+def is_x_mas(text, i, j):
+    square_cut = text[i - 1 : i + 2, j - 1 : j + 2]
+    c1 = square_cut.diagonal()
+    if "MAS" not in ("".join(c1), "".join(reversed(c1))):
+        return False
+    c2 = np.flip(square_cut, axis=0).diagonal()
+    if "MAS" not in ("".join(c2), "".join(reversed(c2))):
+        return False
+    return True
+
+
 answer1 = 0
+answer2 = 0
 for i, j in product(range(text_arr.shape[0]), range(text_arr.shape[1])):
     if text_arr[i, j] == "X":
         answer1 += count_xmas(text_arr, i, j)
+    if text_arr[i, j] == "A":
+        answer2 += is_x_mas(text_arr, i, j)
+
+print(f"{answer1=}") 
+print(f"{answer2=}") 
